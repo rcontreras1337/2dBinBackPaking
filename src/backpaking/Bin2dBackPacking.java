8da24f4809b88f976ce1237232508144fa033588
Ruben Contreras <4tipruben@gmail.com>
2025-01-12 23:40:52 -0300

    Problema solucionado con print grafico Jframe para mostrar resultados uniendo los bloques

--- src/backpaking/Bin2dBackPacking.java
+++ src/backpaking/Bin2dBackPacking.java
@@ -2,6 +2,8 @@ package backpaking;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bin2dBackPacking {
     // Clase para representar un objeto con dimensiones x e y
@@ -23,6 +25,23 @@ public class Bin2dBackPacking {
                 ", width=" + width +
                 ", height=" + height +
                 '}';
+        }
+    }
+
+    // Clase para representar un bloque único
+    static class Block {
+        int id;
+        int startX;
+        int startY;
+        int width;
+        int height;
+
+        public Block(int id, int startX, int startY, int width, int height) {
+            this.id = id;
+            this.startX = startX;
+            this.startY = startY;
+            this.width = width;
+            this.height = height;
         }
     }
 
@@ -71,36 +90,87 @@ public class Bin2dBackPacking {
         return maxPanels;
     }
 
+    // Método para identificar bloques únicos en la matriz
+    public static Map<Integer, Block> identifyBlocks(int[][] space) {
+        Map<Integer, Block> blocks = new HashMap<>();
+
+        for (int i = 0; i < space.length; i++) {
+            for (int j = 0; j < space[0].length; j++) {
+                int id = space[i][j];
+                if (id != 0 && !blocks.containsKey(id)) {
+                    // Encontrar límites del bloque
+                    int startX = i;
+                    int startY = j;
+                    int width = 0;
+                    int height = 0;
+
+                    // Calcular el ancho del bloque
+                    while (j + width < space[0].length && space[startX][j + width] == id) {
+                        width++;
+                    }
+
+                    // Calcular la altura del bloque
+                    while (i + height < space.length && space[i + height][startY] == id) {
+                        height++;
+                    }
+
+                    blocks.put(id, new Block(id, startX, startY, width, height));
+                }
+            }
+        }
+
+        return blocks;
+    }
+
     // Método para mostrar la representación gráfica en una ventana
     public static void displaySpace(int[][] space, int cellSize) {
+        Map<Integer, Block> blocks = identifyBlocks(space);
+
         JFrame frame = new JFrame("Representación de Paneles Solares");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(space[0].length * cellSize + 50, space.length * cellSize + 50);
+        frame.setSize(space[0].length * cellSize + 100, space.length * cellSize + 100);
 
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                for (int i = 0; i < space.length; i++) {
-                    for (int j = 0; j < space[0].length; j++) {
-                        if (space[i][j] != 0) {
-                            g.setColor(Color.CYAN);
-                            g.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
-                            g.setColor(Color.BLACK);
-                            g.drawRect(j * cellSize, i * cellSize, cellSize, cellSize);
-                            g.drawString(String.valueOf(space[i][j]), j * cellSize + cellSize / 4, i * cellSize + cellSize / 2);
-                        } else {
-                            g.setColor(Color.LIGHT_GRAY);
-                            g.drawRect(j * cellSize, i * cellSize, cellSize, cellSize);
-                        }
-                    }
+                // Dibujar fondo del área con bordes
+                g.setColor(Color.WHITE);
+                g.fillRect(30, 30, space[0].length * cellSize, space.length * cellSize);
+                g.setColor(Color.BLACK);
+                g.drawRect(30, 30, space[0].length * cellSize, space.length * cellSize);
+                // Dibujar bloques únicos
+                for (Block block : blocks.values()) {
+                    int x = block.startY * cellSize + 30;
+                    int y = block.startX * cellSize + 30;
+                    int width = block.width * cellSize;
+                    int height = block.height * cellSize;
+
+                    // Asignar color único
+                    g.setColor(new Color((block.id * 50) % 255, (block.id * 80) % 255, (block.id * 110) % 255));
+                    g.fillRect(x, y, width, height);
+
+                    // Dibujar bordes y etiqueta
+                    g.setColor(Color.BLACK);
+                    g.drawRect(x, y, width, height);
+                    g.drawString("ID: " + block.id, x + width / 4, y + height / 2 - 10);
+                    g.drawString(block.width + "x," + block.height + "y", x + width / 4, y + height / 2 + 10);
+                }
+                // Dibujar ejes
+                g.setColor(Color.BLACK);
+                for (int i = 0; i <= space.length; i++) {
+                    g.drawString(String.valueOf(i), 5, i * cellSize + 45);
+                }
+                for (int j = 0; j <= space[0].length; j++) {
+                    g.drawString(String.valueOf(j), j * cellSize + 35, 20);
                 }
             }
         };
 
-        panel.setPreferredSize(new Dimension(space[0].length * cellSize, space.length * cellSize));
+        panel.setPreferredSize(new Dimension(space[0].length * cellSize + 50, space.length * cellSize + 50));
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
     }
+
 }
